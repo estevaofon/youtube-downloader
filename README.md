66390a8b2a0344b7448e7f4c85d55f8c56722821
Estevao <estevaopfon@gmail.com>
2023-05-27 12:58:53 -0300

    🔧 chore(.idea): add .gitignore, profiles_settings.xml, misc.xml, modules.xml, vcs.xml, and youtube-downloader.iml 🎨 style(downloader.py): increase url_frame and option_frame pady, add loading_label and progress bar 🐛 fix(downloader.py): disable url_entry and download_button during download, add progress bar and loading_label animation ✨ feat(downloader.py): add progress_func to update progress bar, add loading_label animation during download, add video completed message box ✨ feat(downloader.py): add audio completed message box, add loading_label animation during download

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This code is a Python script for a YouTube downloader application. It uses the tkinter package to create a user interface with a text entry field for entering a YouTube video URL and radio buttons for selecting video or audio downloads. It also uses the pytube and moviepy packages for downloading and processing the videos. The script runs as a Tkinter application and starts by initializing the UI elements. When the user clicks the download button, the download function retrieves the URL and download option, checks whether both fields are filled, and creates a new thread to start the download process. The download function then passes the URL to the appropriate YouTube download function, either download_video or download_audio, based on the user's selected option. Finally, upon successful completion of a download, an alert box informs the user that the download is completed.
 
+![downloader](downloader.png)
+
 ## :wrench: Requirements
 
 To install the necessary dependencies, run the following command:
--- downloader.py
+++ downloader.py
@@ -4,13 +4,15 @@ from pytube import YouTube
 import threading
 import os
 from moviepy.editor import VideoFileClip, AudioFileClip
+import itertools
+
 
 class Downloader(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title('YouTube Downloader')
-        self.geometry('500x100')
+        self.geometry('500x150')
 
         # Style
         self.style = ttk.Style()
@@ -22,7 +24,7 @@ class Downloader(tk.Tk):
         # UI Elements
         self.url_frame = ttk.Frame(self)
         self.url_label = ttk.Label(self.url_frame, text="YouTube URL:")
-        self.url_entry = ttk.Entry(self.url_frame, width=50)  # Adjust the width here
+        self.url_entry = ttk.Entry(self.url_frame, width=50)
 
         self.option_frame = ttk.Frame(self)
         self.option = tk.StringVar()
@@ -31,15 +33,29 @@ class Downloader(tk.Tk):
 
         self.download_button = ttk.Button(self.option_frame, text="Download", command=self.download)
 
+        self.loading_label = ttk.Label(self.option_frame, text="")
+
+        self.progress = ttk.Progressbar(self, length=400, mode='determinate')
+
         # Pack layout
-        self.url_frame.pack(padx=10, pady=10, fill="both")
+        self.url_frame.pack(padx=10, pady=20, fill="both")
         self.url_label.pack(side=tk.LEFT)
         self.url_entry.pack(side=tk.LEFT)
 
-        self.option_frame.pack(padx=10, pady=10, fill="both")
+        self.option_frame.pack(padx=10, pady=5, fill="both")
         self.radio_mp3.pack(side=tk.LEFT)
         self.radio_mp4.pack(side=tk.LEFT)
         self.download_button.pack(side=tk.LEFT)
+        self.loading_label.pack(side=tk.LEFT)
+
+        self.progress.pack(pady=10)
+
+    def progress_func(self, stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        percentage = (bytes_downloaded / total_size) * 100
+        self.progress['value'] = percentage
+        self.update_idletasks()
 
     def download(self):
         url = self.url_entry.get()
@@ -48,6 +64,8 @@ class Downloader(tk.Tk):
         if not url or not option:
             messagebox.showerror("Error", "All fields must be filled")
             return
+
+        self.progress['value'] = 0
 
         if option == 'mp4':
             self.thread = threading.Thread(target=self.download_video, args=(url,))
@@ -59,6 +77,7 @@ class Downloader(tk.Tk):
     def download_video(self, url):
         try:
             yt = YouTube(url)
+            yt.register_on_progress_callback(self.progress_func)
             video = yt.streams.filter(only_video=True, file_extension='mp4').order_by('resolution').desc().first()
             audio = yt.streams.get_audio_only()
 
@@ -69,26 +88,72 @@ class Downloader(tk.Tk):
             audio_clip = AudioFileClip(audio_file)
 
             final_clip = video_clip.set_audio(audio_clip)
+
+            loading_text = 'Merging Video and Audio...'
+            loading_animation = itertools.cycle(['|', '/', '-', '\\'])
+
+            def animate_loading():
+                self.loading_label.config(text=loading_text + next(loading_animation))
+                if not self.thread.is_alive():
+                    self.download_button.config(state='normal')
+                    self.loading_label.config(text='')
+                else:
+                    self.after(200, animate_loading)
+
+            self.url_entry.config(state='disabled')
+            self.download_button.config(state='disabled')
+            self.loading_label.pack(side=tk.LEFT)
+            self.loading_label.config(text=loading_text + next(loading_animation))
+            self.after(200, animate_loading)
+
             final_clip.write_videofile('final.mp4')
 
             os.remove(video_file)
             os.remove(audio_file)
 
-            messagebox.showinfo("Success", "Download completed")
+            messagebox.showinfo("Success", "Video Completed")
         except Exception as e:
             messagebox.showerror("Error", str(e))
+        finally:
+            self.url_entry.config(state='normal')
+            self.download_button.config(state='normal')
+            self.loading_label.config(text='')
 
     def download_audio(self, url):
         try:
             yt = YouTube(url)
+            yt.register_on_progress_callback(self.progress_func)
             yt = yt.streams.filter(only_audio=True).first()
             out_file = yt.download(filename='audio')
             base, ext = os.path.splitext(out_file)
             new_file = base + '.mp3'
             os.rename(out_file, new_file)
+
+            loading_text = 'Downloading...'
+            loading_animation = itertools.cycle(['|', '/', '-', '\\'])
+
+            def animate_loading():
+                self.loading_label.config(text=loading_text + next(loading_animation))
+                if not self.thread.is_alive():
+                    self.download_button.config(state='normal')
+                    self.loading_label.config(text='')
+                else:
+                    self.after(200, animate_loading)
+
+            self.url_entry.config(state='disabled')
+            self.download_button.config(state='disabled')
+            self.loading_label.pack(side=tk.LEFT)
+            self.loading_label.config(text=loading_text + next(loading_animation))
+            self.after(200, animate_loading)
+
             messagebox.showinfo("Success", "Download completed")
         except Exception as e:
             messagebox.showerror("Error", str(e))
+        finally:
+            self.url_entry.config(state='normal')
+            self.download_button.config(state='normal')
+            self.loading_label.config(text='')
+
 
 if __name__ == "__main__":
     app = Downloader()
